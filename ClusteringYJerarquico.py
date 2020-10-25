1b224810def127cb263a4c355f86352d96292821
David Fonseca <47060767+dgfonseca@users.noreply.github.com>
2020-10-25 10:22:15 -0500

    Cantidad masiva de datos agregada

--- ClusteringYJerarquico.py
+++ ClusteringYJerarquico.py
@@ -5,24 +5,16 @@ import seaborn as sns; sns.set()  # for plot styling
 from sklearn.cluster import KMeans
 import scipy.cluster.hierarchy as shc
 from sklearn.cluster import AgglomerativeClustering
+import numpy as np
 
-df = pd.read_csv(r'C:\Users\david\Desktop\Clases Universidad\Semestre 7\BI\laboratorio_3a_Mall_Customers_Ultima_Version.csv')
+df = pd.read_csv(r'C:\Users\david\Desktop\Clases Universidad\Semestre 7\PMC\ProyectoPMC\proyecto.csv', encoding = "ISO-8859-1")
+print(df.head())
 #Estadisticas
 jd = df
-contadorFem = 0
-contadorMasc = 0
-with open(r'C:\Users\david\Desktop\Clases Universidad\Semestre 7\BI\laboratorio_3a_Mall_Customers_Ultima_Version.csv') as read_obj:
-    csv_reader = reader(read_obj)
-    contadorFem = 0
-    contadorMasc = 0
-    for row in csv_reader:
-        if str(row[1])==str("Male"):
-            contadorMasc+=1
-        else:
-            contadorFem+=1
+cluster_included = df
 
 #K means
-X = df.iloc[:,[3,4]].values
+X = df.iloc[:,[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]].values
 wcss=[]
 
 for i in range(1,11):
@@ -38,34 +30,46 @@ plt.show()
 
 kmeans = KMeans(n_clusters=5, init ='k-means++', max_iter=300, n_init=10,random_state=0 )
 y_kmeans = kmeans.fit_predict(X)
+arreglo = kmeans.predict(X)
+
+clusters = {}
+n=0
+for item in arreglo:
+    if item in clusters:
+        clusters[item].append(df.to_numpy()[n])
+    else:
+        clusters[item] = [df.to_numpy()[n]]
+        n+=1
+for item in clusters:
+    print ("Cluster ", item)
+    for i in clusters[item]:
+        print (i)
+
+plt.scatter(X[y_kmeans==0, 19], X[y_kmeans==0, 20], s=100, c='red', label ='Cluster 1')
+plt.scatter(X[y_kmeans==1, 19], X[y_kmeans==1, 20], s=100, c='blue', label ='Cluster 2')
+plt.scatter(X[y_kmeans==2, 19], X[y_kmeans==2, 20], s=100, c='green', label ='Cluster 3')
+plt.scatter(X[y_kmeans==3, 19], X[y_kmeans==3, 20], s=100, c='black', label ='Cluster 4')
+plt.scatter(X[y_kmeans==4, 19], X[y_kmeans==4, 20], s=100, c='yellow', label ='Cluster 5')
+plt.scatter(X[y_kmeans==5, 19], X[y_kmeans==5, 20], s=100, c='orange', label ='Cluster 6')
 
 
-
-plt.scatter(X[y_kmeans==0, 0], X[y_kmeans==0, 1], s=100, c='red', label ='Cluster 1')
-plt.scatter(X[y_kmeans==1, 0], X[y_kmeans==1, 1], s=100, c='blue', label ='Cluster 2')
-plt.scatter(X[y_kmeans==2, 0], X[y_kmeans==2, 1], s=100, c='green', label ='Cluster 3')
-plt.scatter(X[y_kmeans==3, 0], X[y_kmeans==3, 1], s=100, c='magenta', label ='Cluster 4')
-plt.scatter(X[y_kmeans==4, 0], X[y_kmeans==4, 1], s=100, c='black', label ='Cluster 5')
-
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label = 'Centroids')
-plt.title('Cluster de clientes')
-plt.xlabel('Annual Income(k$)')
-plt.ylabel('Spending Score(1-100')
+plt.scatter(kmeans.cluster_centers_[:, 19], kmeans.cluster_centers_[:, 20], s=300, c='yellow', label = 'Centroids')
+plt.title('Comparación puntaje global vs Proposito en la vivienda')
+plt.xlabel('Puntaje Global')
+plt.ylabel('Proposito universitario')
 plt.show()
 
-#Hierarchical cluster
-data = jd.iloc[:, 3:5].values
-dend = shc.dendrogram(shc.linkage(data, method="ward"))
-cluster = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
-cluster.fit_predict(data)
-plt.figure(figsize=(10, 7))
-plt.title("Dendrograma de los clientes")
-plt.xlabel('Annual Income(k$)')
-plt.ylabel('Spending Score(1-100')
+plt.scatter(X[y_kmeans==0, 0], X[y_kmeans==0, 20], s=100, c='red', label ='Cluster 1')
+plt.scatter(X[y_kmeans==1, 0], X[y_kmeans==1, 20], s=100, c='blue', label ='Cluster 2')
+plt.scatter(X[y_kmeans==2, 0], X[y_kmeans==2, 20], s=100, c='green', label ='Cluster 3')
 
-plt.figure(figsize=(10, 7))
-plt.scatter(data[:,0], data[:,1], c=cluster.labels_, cmap='rainbow')
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 20], s=300, c='yellow', label = 'Centroids')
+plt.title('Comparación puntaje global vs Cantidad compañeros')
+plt.xlabel('Puntaje Global')
+plt.ylabel('Cantida compañeros')
+plt.show()
+
+
 
 print(df.describe().transpose())
-print("Promedio Masculino: " + str(contadorMasc/(contadorFem+contadorMasc)))
-print("Promedio Femenino: " + str(contadorFem/(contadorFem+contadorMasc)))
