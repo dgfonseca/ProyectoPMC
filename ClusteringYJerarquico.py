a96e19ee300ce3375d21090807b0038c3e0d3260
David Fonseca <47060767+dgfonseca@users.noreply.github.com>
2020-10-25 20:56:34 -0500

    Terminado

--- ClusteringYJerarquico.py
+++ ClusteringYJerarquico.py
@@ -28,48 +28,31 @@ plt.xlabel('Numero de clusters')
 plt.ylabel('Suma distancia centroide')
 plt.show()
 
-kmeans = KMeans(n_clusters=5, init ='k-means++', max_iter=300, n_init=10,random_state=0 )
+kmeans = KMeans(n_clusters=3, init ='k-means++', max_iter=300, n_init=10,random_state=0 )
 y_kmeans = kmeans.fit_predict(X)
 arreglo = kmeans.predict(X)
+
 
 clusters = {}
 n=0
 for item in arreglo:
     if item in clusters:
         clusters[item].append(df.to_numpy()[n])
+        n+=1
     else:
         clusters[item] = [df.to_numpy()[n]]
         n+=1
+        
+        
+f = open("analisis.txt","w")
 for item in clusters:
     print ("Cluster ", item)
+    f.write("Cluster"+str(item)+"\n")
     for i in clusters[item]:
-        print (i)
-
-plt.scatter(X[y_kmeans==0, 19], X[y_kmeans==0, 20], s=100, c='red', label ='Cluster 1')
-plt.scatter(X[y_kmeans==1, 19], X[y_kmeans==1, 20], s=100, c='blue', label ='Cluster 2')
-plt.scatter(X[y_kmeans==2, 19], X[y_kmeans==2, 20], s=100, c='green', label ='Cluster 3')
-plt.scatter(X[y_kmeans==3, 19], X[y_kmeans==3, 20], s=100, c='black', label ='Cluster 4')
-plt.scatter(X[y_kmeans==4, 19], X[y_kmeans==4, 20], s=100, c='yellow', label ='Cluster 5')
-plt.scatter(X[y_kmeans==5, 19], X[y_kmeans==5, 20], s=100, c='orange', label ='Cluster 6')
-
-
-plt.scatter(kmeans.cluster_centers_[:, 19], kmeans.cluster_centers_[:, 20], s=300, c='yellow', label = 'Centroids')
-plt.title('Comparación puntaje global vs Proposito en la vivienda')
-plt.xlabel('Puntaje Global')
-plt.ylabel('Proposito universitario')
-plt.show()
-
-plt.scatter(X[y_kmeans==0, 0], X[y_kmeans==0, 20], s=100, c='red', label ='Cluster 1')
-plt.scatter(X[y_kmeans==1, 0], X[y_kmeans==1, 20], s=100, c='blue', label ='Cluster 2')
-plt.scatter(X[y_kmeans==2, 0], X[y_kmeans==2, 20], s=100, c='green', label ='Cluster 3')
-
-
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 20], s=300, c='yellow', label = 'Centroids')
-plt.title('Comparación puntaje global vs Cantidad compañeros')
-plt.xlabel('Puntaje Global')
-plt.ylabel('Cantida compañeros')
-plt.show()
-
+        print (i[0])
+        f.write(str(i[0])+"\n")
+f.close()
+print(n)
 
 
 print(df.describe().transpose())
